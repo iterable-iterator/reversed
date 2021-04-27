b53004f018b8c7c431ce2e7e5f9ae51fba62a9e5
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-27 18:52:24 +0200

    :test_tube: test: Split test execution.

--- test/src/reversed.js
+++ test/src/reversed.js
@@ -2,15 +2,16 @@ import test from 'ava';
 
 import {reversed} from '../../src/index.js';
 
-test('reversed', (t) => {
-	const x = function (A, B) {
-		t.deepEqual(Array.from(reversed(A)), B);
-		t.deepEqual(Array.from(reversed(B)), A);
-	};
+const macro = (t, A, B) => {
+	t.deepEqual(Array.from(reversed(A)), B);
+	t.deepEqual(Array.from(reversed(B)), A);
+};
 
-	x([], []);
-	x([1], [1]);
-	x([1, 2, 3], [3, 2, 1]);
-	x([1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]);
-	x([1, 2, 3, 4, 5, 6, 7, 8, 9], [9, 8, 7, 6, 5, 4, 3, 2, 1]);
-});
+macro.title = (title, A, B) =>
+	title ?? `reversed(${JSON.stringify(A)}) = ${JSON.stringify(B)}`;
+
+test(macro, [], []);
+test(macro, [1], [1]);
+test(macro, [1, 2, 3], [3, 2, 1]);
+test(macro, [1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1]);
+test(macro, [1, 2, 3, 4, 5, 6, 7, 8, 9], [9, 8, 7, 6, 5, 4, 3, 2, 1]);
